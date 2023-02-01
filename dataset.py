86a90ed1da3edc38aeef726945c6c20c930b6a3b
Tony Lay <tony.lay@student.manchester.ac.uk>
2023-02-01 21:14:20 +0000

    implement dataset generator

---
+++ dataset.py
@@ -0,0 +1,68 @@
+import json
+import os
+import math
+import librosa
+import yaml
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+class MFFC_Generator:
+    
+    
+    def __init__(self):
+        # Dictionary to store labels, and MFCCs
+        self.data = {
+            "labels": [],
+            "mfcc": []
+        }    
+
+    # Loads configuration settings
+    def _load_yaml(self):
+        with open("dataset_config.yaml","r") as f:
+            data = yaml.load(f,Loader=yaml.Loader)
+            return data
+
+    # Generates a dataset consisting of labels and MFCCs for the dataset, stored in JSON
+    def generate_dataset(self):
+        config = self._load_yaml()
+        samples_per_segment = int((config["sample_rate"] * config["track_duration"]) / config["segments"])
+        mfcc_vectors_per_segment = math.ceil(samples_per_segment / config["hop_length"])
+
+        for i, (path, _, file_names) in enumerate(os.walk(config["dataset_path"])):
+            if path is not config['dataset_path']:
+                for f in file_names:
+
+                    # Load the audio file
+                    audio_path = os.path.join(path, f)
+
+                    try:
+                        signal, sample_rate = librosa.load(audio_path, sr=config["sample_rate"])
+                    except:
+                        continue
+
+                    # Process all segments of the audio
+                    for j in range(config["segments"]):
+
+                        # Calculate start and finish sample for current segment
+                        start = samples_per_segment * j
+                        finish = start + samples_per_segment
+
+                        # Extract MFCC
+                        mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=config["mfcc"], n_fft=config["n_fft"], hop_length=config["hop_length"])
+                        mfcc = mfcc.T
+
+                        # Store MFCC with expected number of vectors and labels
+                        if len(mfcc) == mfcc_vectors_per_segment:
+                            self.data["mfcc"].append(mfcc.tolist())
+                            self.data["labels"].append(i-1)
+                            print("{}, segment:{}".format(audio_path, j+1))
+
+        # Save MFCCs to a JSON file
+        with open(config["data_path"], "w") as fp:
+            json.dump(self.data, fp, indent=4)       
+        
+        
+if __name__ == "__main__":
+    mfcc_generator = MFFC_Generator()
+    mfcc_generator.generate_dataset()
